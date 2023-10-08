1f8d925919492eb48afc893c22315d872944cb71
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-08 20:57:50 -0300

    att

--- rnp-idiomas/app/components/Navbar.tsx
+++ rnp-idiomas/app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Navbar() {
   return <>
   
-  <div className="container flex justify-between align-middle items-end p-5 ">
+  <div className="flex justify-between align-middle items-end p-5 ">
           <Image src={require("../src/images/359464103_757245303077001_5758154635758079026_n.jpg")} alt='icon'
           className='w-10' />
           <nav>
--- rnp-idiomas/app/page.tsx
+++ rnp-idiomas/app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return <>
-  <div className='container'>
+  <div className=''>
     <div className="w-full flex justify-center bg-red-400">
       <h1>HOME PAGE</h1>
     </div>
     {/*Banner section*/}
-    <section className=" flex justify-center bg-blue-400">
-      <div className="container flex w-auto bg-white">
+    <section className=" flex  min-w-min justify-center bg-blue-400">
+      <div className="flex w-auto bg-white">
         <Image src={require("./src/images/BO_9020-1024x683.jpg")}
         alt="img"
         className="h-1/2"
@@ -19,11 +19,14 @@ export default function Home() {
 
     </section>
     {/*Banner section*/}
-    <section className="container">
-      <div>
-        <div>
+    <section className="flex justify-center bg-blue-400">
+      <div className="flex w-full text-sm">
+        <div className="bg-white">
           <h1>lorem h1</h1>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, quo iure nobis alias reprehenderit quibusdam, reiciendis quaerat quis labore ratione eligendi iste vel ex eaque velit, illo ipsam beatae aperiam!</p>
+        </div>
+        <div>
+          <Image src={require("./src/images/rec-n-play-2022-3.jpg")}/>
         </div>
         
       </div>
