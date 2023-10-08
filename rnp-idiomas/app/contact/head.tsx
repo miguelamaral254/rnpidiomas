655a21b3108ddc458fbd415fe2c948045fb38a04
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-07 23:07:41 -0300

    Nav done

---
+++ rnp-idiomas/app/contact/head.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ContactHead() {
+  return <>
+  <title>Contact Head</title>
+  </>
+    
+  
+}
---
+++ rnp-idiomas/app/news/head.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewsHead() {
+  return <>
+  <title>News Head</title>
+  </>
+    
+  
+}
