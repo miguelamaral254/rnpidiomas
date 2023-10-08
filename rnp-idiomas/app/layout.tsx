cb6ed54652c4cbd1df3dc01c31fc2e49033e67b7
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-07 22:51:07 -0300

    att
    Created - navbar

--- rnp-idiomas/app/layout.tsx
+++ rnp-idiomas/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>Pagina idiomas</h1>
+        <nav >
+          <ul className= "flex gap-2">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/users">Sobre</Link></li>
+            <li><Link href="/news">Notícias</Link></li>
+            <li><Link href="projects">Projetos</Link></li>
+            <li><Link href="contact">Contato</Link></li>
+          </ul>
+        </nav>
+        <hr />
+        {children}</body>
     </html>
   )
 }
