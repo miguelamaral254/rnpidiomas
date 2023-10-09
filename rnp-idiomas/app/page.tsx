8144ba1e48ac791921b563185d50a29cca184752
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-08 21:24:37 -0300

    Update page.tsx

--- rnp-idiomas/app/page.tsx
+++ rnp-idiomas/app/page.tsx
@@ -1,38 +1,60 @@
 import Image from "next/image";
 
-
 export default function Home() {
-  return <>
-  <div className=''>
-    <div className="w-full flex justify-center bg-red-400">
-      <h1>HOME PAGE</h1>
-    </div>
-    {/*Banner section*/}
-    <section className=" flex  min-w-min justify-center bg-blue-400">
-      <div className="flex w-auto bg-white">
-        <Image src={require("./src/images/BO_9020-1024x683.jpg")}
-        alt="img"
-        className="h-1/2"
-        />
+  return (
+    <div className="container mx-auto">
+      <div className="w-full flex justify-center bg-red-400">
+        <h1>HOME PAGE</h1>
       </div>
       
-
-    </section>
-    {/*Banner section*/}
-    <section className="flex justify-center bg-blue-400">
-      <div className="flex w-full text-sm">
-        <div className="bg-white">
-          <h1>lorem h1</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, quo iure nobis alias reprehenderit quibusdam, reiciendis quaerat quis labore ratione eligendi iste vel ex eaque velit, illo ipsam beatae aperiam!</p>
+      {/* Banner section */}
+      <section className="flex justify-center bg-blue-400">
+        <div className="w-full md:w-2/3 lg:w-1/2">
+          <Image
+            src={require("./src/images/IMG_6126-1035x425.jpeg")}
+            alt="img"
+            className="w-auto"
+          />
         </div>
-        <div>
-          <Image src={require("./src/images/rec-n-play-2022-3.jpg")}/>
+      </section>
+      {/* End Banner section */}
+      
+      {/* Content section */}
+      <section className="flex justify-center bg-blue-400">
+        <div className="w-full md:w-2/3 lg:w-1/2 bg-white p-4">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2">
+            Lorem Ipsum
+          </h1>
+          <p className="text-sm md:text-base lg:text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, quo
+            iure nobis alias reprehenderit quibusdam, reiciendis quaerat quis
+            labore ratione eligendi iste vel ex eaque velit, illo ipsam beatae
+            aperiam!
+          </p>
         </div>
-        
-      </div>
-    </section>
-
-
-  </div>
-  </>
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <Image src={require("./src/images/rec-n-play-2022-3.jpg")} alt="img" />
+        </div>
+      </section>
+      {/* End Content section */}
+      {/* Content section */}
+      <section className="flex justify-center bg-blue-400">
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <Image src={require("./src/images/rec-n-play-2022-3.jpg")} alt="img" />
+        </div>
+        <div className="w-full md:w-2/3 lg:w-1/2 bg-white p-4">
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold mb-2">
+            Lorem Ipsum
+          </h1>
+          <p className="text-sm md:text-base lg:text-lg">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, quo
+            iure nobis alias reprehenderit quibusdam, reiciendis quaerat quis
+            labore ratione eligendi iste vel ex eaque velit, illo ipsam beatae
+            aperiam!
+          </p>
+        </div>
+      </section>
+      {/* End Content section */}
+    </div>
+  );
 }
