e28db9bad5097750ab2e6d319a461e5caa48da80
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2023-10-08 22:54:16 -0300

    Update Carousel.tsx

--- rnp-idiomas/app/components/Carousel.tsx
+++ rnp-idiomas/app/components/Carousel.tsx
@@ -4,28 +4,35 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+const images = [
+  {
+    src: require("../src/images/rec-n-play-2022-3.jpg"),
+    alt: "Image 1",
+    width: 400,
+    height: 100,
+  },
+  {
+    src: require("../src/images/rec-n-play-2022-3.jpg"),
+    alt: "Image 2",
+    width: 400,
+    height: 100,
+  },
+  // Adicione mais imagens conforme necessário
+];
+
 const CarouselComponent: React.FC = () => {
   return (
     <Carousel>
-      <div>
-        <Image
-          src={require("../src/images/rec-n-play-2022-3.jpg")}
-          alt="Image 1"
-          width={300} 
-          height={200}
-        />
-        <p>Legenda do Slide 1</p>
-      </div>
-      <div>
-        <Image
-          src={require("../src/images/rec-n-play-2022-3.jpg")}
-          alt="Image 2"
-          width={300} 
-          height={200}
-        />
-        <p>Legenda do Slide 2</p>
-      </div>
-      {/* Adicione mais slides conforme necessário */}
+      {images.map((image, index) => (
+        <div key={index}>
+          <Image
+            src={image.src.default} // Use .default para acessar o valor retornado por require
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+          />
+        </div>
+      ))}
     </Carousel>
   );
 };
